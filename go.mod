79b165a343caa39e8976319eb4c6d3e75e466bb6
Daniel <eclipsemod-post@yandex.ru>
2025-01-31 11:38:20 +0300

    initial

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/eclipsemode/go-yookassa-sdk
+
+go 1.23.5
+
+require go.uber.org/zap v1.27.0
+
+require go.uber.org/multierr v1.11.0 // indirect
---
+++ yookassa/payments.go
@@ -0,0 +1,35 @@
+package yookassa
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/eclipsemode/go-yookassa-sdk/yookassa/model"
+	"go.uber.org/zap"
+	"net/http"
+)
+
+type PaymentsSvc struct {
+	*Client
+	log *zap.SugaredLogger
+}
+
+func NewPaymentsService(client *Client, log *zap.SugaredLogger) *PaymentsSvc {
+	return &PaymentsSvc{
+		client,
+		log,
+	}
+}
+
+func (h *PaymentsSvc) CreatePayment(payment *model.Payment, idempotencyKey string) (*http.Response, error) {
+	pJSON, err := json.Marshal(payment)
+	if err != nil {
+		return nil, fmt.Errorf("error marshalling payment json: %w", err)
+	}
+
+	res, err := h.makeRequest(http.MethodPost, PaymentEndpoint, "", pJSON, nil, idempotencyKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
